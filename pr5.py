9b3a987d2ba418053630bde181483f1c628fd65a
Strekalovskaya Dinara <kuragatan0@gmail.com>
2025-12-24 16:18:59 +0900

    Обработка символьных строк
    23.12.2025

--- pr5.py
+++ pr5.py
@@ -1,63 +1,145 @@
-# Задание 1. Функция факториала числа
+# Рекурсивные функции 2
+# Задание 1. Исполнитель n преобразует число, записанное на экрне
+# У исполнителя есть три команды:
+# 1. Прибавить 1
+# 2. Прибавить 2
+# 3. Умножить на 2
 """
-def factorial(n):
-    if n > 1:
-        return n * factorial(n - 1)
-    return 1
-print(factorial(8))
+def komanda(n, nomer):
+    if nomer == 1:
+        return n + 1
+    if nomer == 2:
+        return n + 2
+    if nomer == 3:
+        return n * 2
+
+def kolvo(start, finish):
+    if start > finish:
+        return 0
+    if start == finish:
+        return 1
+    return (kolvo(komanda(start, 1), finish) + kolvo(komanda(start, 2), finish) + kolvo(komanda(start, 3), finish))
+
+otvet = kolvo(3, 10) * kolvo(10, 12)
+
+print(kolvo(3, 10))
+print(kolvo(10, 12))
+print(otvet)
 """
 
-# Задание 2. Функция, которая принимает и возвращает согласные буквы
+# Задание 2. Исполнитель f преобразует число на экране
+# У исполнителя f две команды:
+# 1. Прибавь 1
+# 2. Сделай нечетное
 """
-def remove_vowels(string):
-    bukvi = "aeiouAEIOU"
+def komanda(f, nomer):
+    if nomer == 1:
+        return f + 1
+    if nomer == 2:
+        return 2 * f + 1
 
-    if len(string) == 0:
-        return ""
-    stroka = string[0]
-    srez = string[1:]
+def kolvo(start, finish):
+    if start == 26:
+        return 0
+    if start > finish:
+        return 0
+    if start == finish:
+        return 1
+    return (kolvo(komanda(start, 1), finish) + kolvo(komanda(start, 2), finish))
 
-    if stroka in bukvi:
-        return remove_vowels(srez)
+print(kolvo(1, 27))
+"""
+
+# Задание 3. Исполнитель s преобразует число на экране
+# У исполнителя есть две команды:
+# 1. Прибавить 1
+# 2. Прибавить 2
+"""
+def komanda(s, nomer):
+    if nomer == 1:
+        return s + 1
+    if nomer == 2:
+        return s + 2
+
+def kolvo(start, finish):
+    if start == 14:
+        return 0
+    if start > finish:
+        return 0
+    if start == finish:
+        return 1
+
+    return (kolvo(komanda(start, 1), finish) + kolvo(komanda(start, 2), finish))
+
+otvet = kolvo(2, 9) * kolvo(9, 18)
+print(otvet)
+"""
+
+# Обработка символьных строк
+# Задание 1. Определить длину самой длинной последовательности X
+"""
+with open("27686.txt", "r") as f:
+    n = f.readlines()
+max = 0
+count = 0
+
+for i in n:
+    for symbol in i:
+        if symbol == 'X':
+            count += 1
+            if count > max:
+                max = count
+        else:
+            count = 0
+print(max) 
+"""
+
+# Задание 2. Максимальное количество идущих подряд символов без XZZY
+"""
+with open("36037.txt", "r") as f:
+    n = f.readlines()
+max = 0
+count = 0
+text = ""
+
+for i in n:
+    text += i
+
+for i in range(len(text) - 3):
+    if text[i] == 'X' and text[i + 1] == 'Z' and text[i + 2] == 'Z' and text[i + 3] == 'Y':
+        if count > max:
+            max = count
+        count = 3
     else:
-        return stroka + remove_vowels(srez)
-print(remove_vowels("Hello PineApple Apple Pen"))
+        count += 1
+        if count > max:
+            max = count
+print(max)
 """
 
-# Задание 3. Функция вывода строки треугольника Паскаля
+# Задание 3. Количество групп из идущих подряд не менее 12 символов,
+# которые начинаются и заканчиваются буквой E и не содержат других букв E и букв F
 """
-def pascal(n):
-    if n == 1:
-        return [1]
-    prev = pascal(n - 1)
-    stroka = [1]
-    
-    for i in range(len(prev) - 1):
-        stroka.append(prev[i] + prev[i + 1])
-    
-    stroka.append(1)
-    return stroka
-print(pascal(10))
+with open("46982.txt", "r") as f:
+    n = f.readlines()
+
+s = []
+for i in n:
+    for j in i:
+        if j != "":
+            s.append(j)
+count = 0
+
+for i in range(len(s)):
+    if s[i] == "E":
+        for j in range(i + 1, len(s)):
+            if s[j] == "F":
+                break
+
+            if s[j] == "E":
+                dlina = j - i + 1
+                if dlina >= 12:
+                    count = count + 1
+                break
+print(count)
 """
-
-# Задание 4. Лабиринт для героя
-
-def solve(maze):
-    koordinata_x = 0
-    koordinata_y = 0
-    for y in range(len(maze)):
-        for x in range(len(maze[0])):
-            if maze[y][x] == 's':
-                koordinata_x = x
-                koordinata_y = y
-
-robottut = []
-
-maze = [
-    's----',
-    '##---',
-    '---##',
-    '----x'
-]
-
-print(solve(maze))
